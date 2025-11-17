55f983d7e5c9bc4be3e86b2719d69cd766a88e9a
The Buildbot <buildbot@angular2buch.de>
2025-11-17 19:26:23 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/buch/commit/8fd27f94eeeeb010f4af7db5e9b367ae6bbebeaf

--- 14-lazyloading/chunk-KFCGO2MW.js
+++
@@ -1,1 +0,0 @@
-import{Aa as o,Va as a,Wa as n,db as m}from"./chunk-KP2HMDDQ.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-home-page"]],decls:2,vars:0,template:function(e,s){e&1&&(a(0,"h1"),m(1,"Welcome to the BookMonkey!"),n())},encapsulation:2})};export{i as HomePage};
